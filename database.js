eab32c4aa54863c43999f7dea49c3871279a16f1
Joe Jones <joeknockando@googlemail.com>
2014-11-07 09:11:09 +0000

    New way of creating ids.

--- database.js
+++ database.js
@@ -143,7 +143,7 @@ Database.prototype = {
 	*/
 	
 	createID: function() {
-		return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+		return Date().replace(/[\s+():]/g, "") + (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 	},
 	
 	keys: function() {
