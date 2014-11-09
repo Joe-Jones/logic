4a7ab2ad2697fb969aebac6d9f1c018cfc9733cb
Joe Jones <joeknockando@googlemail.com>
2014-11-09 17:03:02 +0000

    1) load jquery underscore backbone and sylvester from CDNs. Updating to the latest version of each library.
    2) remove these files from git.
    
    3) Some work towards a minified version.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-cat underscore.js jquery.js backbone.js LogicSystem.js Geometory.js Items.js SchemaModel.js SchemaDrawer.js SchemaView.js LogicWidget.js Project.js logic.js > all-javascript.js
+cat w2ui.js jake-kit.js database.js LogicSystem.js Geometory.js Items.js SchemaModel.js SchemaDrawer.js SchemaView.js LogicWidget.js Project.js logic.js > all-javascript.js
 java -jar closure/compiler.jar --js all-javascript.js --js_output_file logic-compiled.js
