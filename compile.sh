63fba562a51d7fc0994654270ba5e585b24beea3
Joe Jones <joeknockando@googlemail.com>
2014-11-09 17:14:29 +0000

    Combined css file.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 cat w2ui.js jake-kit.js database.js LogicSystem.js Geometory.js Items.js SchemaModel.js SchemaDrawer.js SchemaView.js LogicWidget.js Project.js logic.js > all-javascript.js
+cat w2ui.css logic.css jake-kit.css > combined.css
 java -jar closure/compiler.jar --js all-javascript.js --js_output_file logic-compiled.js
